4f04be3f3dcd02eb33b2d8f34bf2b46822327c79
if25b180 <if25b180@technikum-wien.at>
2026-05-18 17:30:25 +0200

    pass 4th test -> now 3rd test fails

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,4 +1,13 @@
 function Spaceship() { }
 Spaceship.prototype.find = function(map) {
+	for (let x = 0; x < map[0].length; x++) {
+		for (let y = 0; y < map.length; y++) {
+			const value = map[y][x];
+			if (value == 'X') {
+				return [x, y - 1];
+			}
+		}
+	}
+
 	return "Spaceship lost forever.";
 };
