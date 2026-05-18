3af18384a20c8362bf68503c1d9c9bdc7a0dd8b1
if25b180 <if25b180@technikum-wien.at>
2026-05-18 17:32:54 +0200

    pass all other tests with this guard clause

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,5 +1,9 @@
 function Spaceship() { }
 Spaceship.prototype.find = function(map) {
+	if (!Array.isArray(map) || map.length <= 0) {
+		return "Spaceship lost forever.";
+	}
+
 	for (let x = 0; x < map[0].length; x++) {
 		for (let y = 0; y < map.length; y++) {
 			const value = map[y][x];
