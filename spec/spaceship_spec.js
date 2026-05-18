763630788541cf97e0b0ec5c054491bbd7528112
if25b180 <if25b180@technikum-wien.at>
2026-05-18 17:19:35 +0200

    write second test: empty map
    second test passes already by accident

--- spec/spaceship_spec.js
+++ spec/spaceship_spec.js
@@ -9,10 +9,16 @@ describe('Spaceship', function() {
 			['.','.','.','.','.','.','.','.','.','.'],
 			['.','.','.','.','.','.','.','.','.','.'],
 			['.','.','.','.','.','.','.','.','.','.'],
-		]
+		];
+
+		mapEmpty = [];
 	});
 
 	it('should return "Spaceship lost forever." on a map without X ', function() {
 		expect(spaceship.find(mapWithoutX)).toEqual("Spaceship lost forever.");
+	});
+
+	it('should return "Spaceship lost forever." on an empty map', function() {
+		expect(spaceship.find(mapEmpty)).toEqual("Spaceship lost forever.");
 	});
 });
